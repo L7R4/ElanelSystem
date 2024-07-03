cb60a17aad7ab0c6778eff09bb1170a4ba18fee4
Lautaro Rodriguez <lautaro.rodriguez553@gmail.com>
2024-07-03 19:55:38 -0300

    a

---
+++ elanelsystem/static/js/errorsFormManagement.js
@@ -0,0 +1,31 @@
+function mostrarErrores(errores,formulario) {
+    // Limpiar los mensajes de error anteriores
+    let messagesError = document.querySelectorAll('.message-error');
+    messagesError.forEach(mensaje => mensaje.remove());
+
+    // Recorrer el objeto de errores
+    for (let [name, mensaje] of Object.entries(errores)) {
+        // Buscar el input correspondiente por su atributo name
+        const input = formulario.querySelector(`input[name="${name}"]`);
+        if (input) {
+            // Crear una etiqueta p para el mensaje de error
+            const p = document.createElement('p');
+            p.textContent = mensaje;
+            p.classList.add('message-error'); // Clase para estilos
+
+            // Insertar la etiqueta p después del input
+            input.insertAdjacentElement('afterend', p);
+        }
+    }
+}
+
+// Ejemplo de uso
+const errores = {
+    nombre: "El nombre es obligatorio.",
+    email: "El email no es válido.",
+    telefono: "El teléfono debe tener 10 dígitos."
+};
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     mostrarErrores(errores);
+// });
--- elanelsystem/static/js/redirectToPDFNewUser.js
+++ elanelsystem/static/js/redirectToPDFNewUser.js
@@ -1,6 +1,6 @@
 let confirmNewUser = document.getElementById("saveButton");
 let urlCreateUser = window.location.pathname
-const formCreateUser = document.getElementById("form_create_user")
+const form_create_user = document.getElementById("form_create_user")
 
 function getCookie(name) {
     let cookieValue = null;
@@ -17,26 +17,52 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
-confirmNewUser.addEventListener("click",()=>{
-    newUser().then(data =>{
-        if(data.success){
-            window.open(data.urlPDF, '_blank');
-            window.location.href = data.urlRedirect 
+async function fetchCRUD(body, url) {
+    try {
+        let response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "X-CSRFToken": getCookie('csrftoken'),
+            }
+        })
+        if (!response.ok) {
+            throw new Error("Error")
         }
-    })
-    .catch(error => {})
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error en fetchCRUD:", error); // Agregar este log
+    }
+}
+
+
+confirmNewUser.addEventListener("click",async()=>{
+    let body = {
+        nombre: form_create_user.nombre.value,
+        dni: form_create_user.dni.value,
+        tel: form_create_user.tel.value,
+        fec_nacimiento: form_create_user.fec_nacimiento.value,
+        domic: form_create_user.domic.value,
+        prov: form_create_user.prov.value,
+        loc: form_create_user.loc.value,
+        cp: form_create_user.cp.value,
+        lugar_nacimiento: form_create_user.lugar_nacimiento.value,
+        estado_civil: form_create_user.estado_civil.value,
+        xp_laboral: form_create_user.xp_laboral.value,
+        sucursal: form_create_user.sucursal.value,
+        rango: form_create_user.rango.value,
+        fec_ingreso: form_create_user.fec_ingreso.value,
+        email: form_create_user.email.value,
+        password: form_create_user.password.value
+    }
+
+    let response = await fetchCRUD(body,urlCreateUser)
+    if(!response["success"]){
+        mostrarErrores(response["errors"],form_create_user)
+    }
+
 })
 
-async function newUser() {
-    let postNewUser = await fetch(urlCreateUser,{
-        method: "POST",
-        body: new FormData(formCreateUser),
 
-        headers: {
-            "X-CSRFToken": getCookie('csrftoken'),
-        }
-    })
-    let data = await postNewUser.json()
-    return data;
-}
+
