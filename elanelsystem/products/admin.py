93c5327ac438947c5377336f9bda147452fa5474
Rodriguez Lautaro <lautaro.rodriguez553@gmail.com>
2025-04-03 09:55:33 -0300

    a

--- elanelsystem/products/admin.py
+++ elanelsystem/products/admin.py
@@ -3,8 +3,8 @@ from .models import Plan, Products
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ("tipo_de_producto","nombre","plan")
+    list_display = ("tipo_de_producto","tipodePlan","nombre","plan")
 
 @admin.register(Plan)
 class PlansAdmin(admin.ModelAdmin):
-    list_display = ("valor_nominal","tipodePlan", 'suscripcion', 'primer_cuota', 'c24_porcentage', 'c30_porcentage','c48_porcentage','c60_porcentage') 
+    list_display = ("valor_nominal", 'suscripcion', 'primer_cuota', 'c24_porcentage', 'c30_porcentage','c48_porcentage','c60_porcentage') 
