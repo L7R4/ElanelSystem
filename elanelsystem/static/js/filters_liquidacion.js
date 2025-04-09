25fe6af4fa5c71b4c0810436277d925e6834a17e
Lautaro Rodriguez <lautaro.rodriguez553@gmail.com>
2025-04-09 11:51:26 -0300

    asd

--- elanelsystem/static/js/filters_liquidacion.js
+++ elanelsystem/static/js/filters_liquidacion.js
@@ -143,84 +143,12 @@ function actualizarResultadosColaboradores(resultados, contenedor) {
             <p>$ ${item.comisionTotal}</p>
         </div>
         <div>
-            <button type="button" class="button-default-style" id="liquidarButton" onclick="modal_ajuste_comision(${item.id}, '${item.nombre}')">Ajustar comision</button>
+            <button type="button" class="button-default-style" id="liquidarButton" onclick="modal_ajuste_comision(${item.id}, '${item.nombre}', ${item.comisionTotal})">Ajustar comision</button>
         </div>
     </li>`;
     contenedor.insertAdjacentHTML("beforeend",divs)
   
 });
-}
-
-function render_form_ajuse_comision(nombre_usuario) {
-    return `<form method="POST" class="modal_form" id="formNewMov">
-
-            <div class="wrapperTittle">
-                <h3 class="labelInput">Ajustar comisiones a ${nombre_usuario}</h3>
-            </div>
-            ${CSRF_TOKEN}
-
-            <div class="wrapperInput">
-                <h3 class="labelInput">Dinero</h3>
-                <input type="number" name="dinero" id="dineroAjusteInput" class="input-read-write-default">
-            </div>
-            <div class="wrapperInputObservaciones">
-                <label for="observacionesInput">Observaciones</label>
-                <textarea name="observaciones" id="observacionesInput" cols="30" rows="10"></textarea>
-            </div>
-        </form>`
-}
-
-function modal_ajuste_comision(id_usuario, nombre_usuario){
-    let modal = new tingle.modal({
-        footer: true,
-        closeMethods: [''],
-        cssClass: ['moda_container_ajusteComision'],
-
-        onOpen: function () {
-            initCustomSingleSelects()
-        },
-
-        onClose: function () {
-            modal.destroy();
-        },
-    });
-    console.log(id_usuario, nombre_usuario)
-
-
-    // set content
-    modal.setContent(render_form_ajuse_comision(nombre_usuario));
-
-
-    // add a button
-    modal.addFooterBtn('Guardar', 'tingle-btn tingle-btn--primary', async function () {
-
-        body = {
-            "campania": document.querySelector("#campaniaInput").value,
-            "agencia": document.querySelector("#sucursalInput").value,
-        }
-        console.log(body)
-        // showLoader()
-        // let response = await fetchCRUD(body, urlLiquidacion)
-
-        // if (response.status) {
-        //     console.log("Salio todo bien");
-        //     hiddenLoader();
-        //     modal.close();
-        //     modal.destroy();
-        // }else{
-        //     console.log("Salio mal")
-        //     hiddenLoader()
-        //     modal.close();
-        //     modal.destroy();
-        // }
-    });
-
-    modal.addFooterBtn('Cancelar', 'tingle-btn tingle-btn--default', function () {
-        modal.close();
-        modal.destroy();
-    });
-
-    modal.open();
 }
 
 function actualizarTotalComisionado(dinero){
