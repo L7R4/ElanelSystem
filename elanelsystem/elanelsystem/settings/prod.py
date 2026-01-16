20e25c0933b9c7fa9fcd7ea03b87722dd2d9d1cd
Lautaro Rodriguez <lautaro.rodriguez553@gmail.com>
2026-01-16 15:42:22 -0300

    cambios

--- elanelsystem/elanelsystem/settings/prod.py
+++ elanelsystem/elanelsystem/settings/prod.py
@@ -3,6 +3,21 @@ from .base import *
 # —— Producción —— 
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://elanelsys.com",
+    "https://www.elanelsys.com",
+    "https://site.elanelsys.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True  # opcional pero suele ayudar detrás de proxy
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = "Lax"
+SESSION_COOKIE_SAMESITE = "Lax"
+
 # BD real (Postgres, MySQL…)
 DATABASES = {
     'default': {
